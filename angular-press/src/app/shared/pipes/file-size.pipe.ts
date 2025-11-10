3be714718f268438f342149bec8780cfd53c472b
Benjamin Hill <senneseph@gmail.com>
2025-11-09 19:16:18 -0500

    shared module and file size pipe

---
+++ angular-press/src/app/shared/pipes/file-size.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'fileSize'
+})
+export class FileSizePipe implements PipeTransform {
+  transform(bytes: number): string {
+    if (bytes === 0) return '0 B';
+
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+  }
+}
--- angular-press/src/app/shared/shared.module.ts
+++ angular-press/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FileSizePipe } from './pipes/file-size.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FileSizePipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    FileSizePipe
   ]
 })
 export class SharedModule { }
