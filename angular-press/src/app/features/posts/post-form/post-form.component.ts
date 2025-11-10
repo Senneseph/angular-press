b563d8eab66670331c77d6f8875049b0a22dffb9
Benjamin Hill <senneseph@gmail.com>
2025-11-09 19:08:59 -0500

    Post form component going in

---
+++ angular-press/src/app/features/posts/post-form/post-form.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Post } from '../../../core/models/post.interface';
+import { PostActions } from '../../../store/posts/posts.state';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.scss']
+})
+export class PostFormComponent implements OnInit {
+  postForm: FormGroup;
+  isEditMode = false;
+  postId: string | null = null;
+  editorConfig = {
+    height: '400px',
+    menubar: true,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar:
+      'undo redo | formatselect | bold italic backcolor | \
+      alignleft aligncenter alignright alignjustify | \
+      bullist numlist outdent indent | removeformat | help'
+  };
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.postForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+      excerpt: [''],
+      status: ['draft'],
+      tags: [''],
+      categories: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.postId = this.route.snapshot.paramMap.get('id');
+    if (this.postId) {
+      this.isEditMode = true;
+      // TODO: Load post data and populate form
+    }
+  }
+
+  onSubmit() {
+    if (this.postForm.valid) {
+      const postData: Post = {
+        ...this.postForm.value,
+        id: this.postId || undefined,
+        author: 'Current User', // TODO: Get from auth service
+        publishDate: new Date(),
+        featured_image: '',
+        modified: new Date()
+      };
+
+      if (this.isEditMode) {
+        this.store.dispatch(new PostActions.UpdatePost(postData));
+      } else {
+        this.store.dispatch(new PostActions.CreatePost(postData));
+      }
+
+      this.router.navigate(['/posts']);
+    }
+  }
+}
